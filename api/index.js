f7c34ec1cd14a5e55766399719eb69705a7c8fb9
Faizanybo <faizan@yboagency.com>
2026-09-17 21:53:46 +0500

    Fix ServerlessWP Expect header handling

--- api/index.js
+++ api/index.js
@@ -1,1 +1,19 @@
-exports.handler = require('serverlesswp/wordpress').handler;
+const { handler: wordpressHandler } = require('serverlesswp/wordpress');
+
+function stripExpectHeader(event) {
+    if (!event || !event.headers) {
+        return event;
+    }
+    for (const name of Object.keys(event.headers)) {
+        if (name.toLowerCase() === 'expect') {
+            delete event.headers[name];
+        }
+    }
+    return event;
+}
+
+async function handler(event, context, callback) {
+    return wordpressHandler(stripExpectHeader(event), context, callback);
+}
+
+exports.handler = handler;
--- api/vercel.js
+++ api/vercel.js
@@ -1,4 +1,20 @@
-const { handler } = require('serverlesswp/wordpress');
+const { handler: wordpressHandler } = require('serverlesswp/wordpress');
+
+function stripExpectHeader(event) {
+    if (!event || !event.headers) {
+        return event;
+    }
+    for (const name of Object.keys(event.headers)) {
+        if (name.toLowerCase() === 'expect') {
+            delete event.headers[name];
+        }
+    }
+    return event;
+}
+
+async function handler(event, context, callback) {
+    return wordpressHandler(stripExpectHeader(event), context, callback);
+}
 
 module.exports = handler;
 module.exports.handler = handler;
